64912345529efa1693cd3410ddfc4c8e3adf2c7a
Alex B <axbaretto@gmail.com>
2018-04-15 22:01:46 -0700

    DRILL-6296: Add operator metrics for batch sizing for merge join
    DRILL-6284: Add operator metrics for batch sizing for flatten
    
    DRILL-6230: Extend row set readers to handle hyper vectors
    
    DRILL-6016: Fix for Error reading INT96 created by Apache Spark
    
    DRILL-6271: Updated copyright range in NOTICE
    
    DRILL-6287: apache-release profile should be disabled by default
    
    DRILL-6303: Provide a button to copy the Drillbit's JStack shown in /threads
    
    Provides a button in the web UI to copy the thread stack to the user's clipboard.
    
    DRILL-6279: Indicate operators that spilled in-memory data to disk on Web UI
    
    As part of this coomit added Bootstrap's Glyphicons.
    
    DRILL-6234: Improved documentation for VariableWidthVector mutators, and added simple unit tests demonstrating mutator behavior.
    
    DRILL-6103: lsb_release: command not found
    
    DRILL-6259: Support parquet filter push down for complex types
    
    DRILL-6125: Fix possible memory leak when query is cancelled or finished.
    
    DRILL-6256: Remove references to java 7 from readme and other files
    
    DRILL-6278: Removed temp codegen directory in testing framework.
    
    DRILL-6254: IllegalArgumentException: the requested size must be non-negative
    
    DRILL-5937: drill-module.conf : Changed timeout to 30 seconds, ExecConstant.java : Changed comment
    
    DRILL-5937: ExecConstant.java : removed comment
    
    DRILL-5937: CheckStyle fix
    
    DRILL-6299: Fixed a filter pushed down issue when a column doesn't have stats
    
    DRILL-6300: Refresh protobuf C++ source files
    
    DRILL-6288: Upgrade org.javassist:javassist and org.reflections:reflections
    
    DRILL-6290: Refactor TestInfoSchemaFilterPushDown tests to use PlanTestBase utility methods
    
    DRILL-6283: WebServer stores SPNEGO client principal without taking any conversion rule
    
    DRILL-6280: Cleanup execution of BuildTimeScan during maven build
    
    DRILL-6262: IndexOutOfBoundException in RecordBatchSize for empty variableWidthVector

--- exec/vector/src/main/java/org/apache/drill/exec/vector/accessor/ColumnReader.java
+++ exec/vector/src/main/java/org/apache/drill/exec/vector/accessor/ColumnReader.java
@@ -17,48 +17,67 @@
  */
 package org.apache.drill.exec.vector.accessor;
 
-import java.math.BigDecimal;
-
-import org.joda.time.Period;
+import org.apache.drill.exec.record.metadata.ColumnMetadata;
 
 /**
- * Defines a reader to obtain values from value vectors using
- * a simple, uniform interface. Vector values are mapped to
- * their "natural" representations: the representation closest
- * to the actual vector value. For date and time values, this
- * generally means a numeric value. Applications can then map
- * this value to Java objects as desired. Decimal types all
- * map to BigDecimal as that is the only way in Java to
- * represent large decimal values.
- * <p>
- * In general, a column maps to just one value. However, derived
- * classes may choose to provide type conversions if convenient.
- * An exception is thrown if a call is made to a method that
- * is not supported by the column type.
- * <p>
- * Values of scalars are provided directly, using the get method
- * for the target type. Maps and arrays are structured types and
- * require another level of reader abstraction to access each value
- * in the structure.
+ * Base interface for all column readers, defining a generic set of methods
+ * that all readers provide. In particular, given the metadata and the object
+ * type, one can determine what to do with each reader when working with readers
+ * generically. The <tt>getObject()</tt> and <tt>getAsString()</tt> methods provide
+ * generic data access for tests and debugging.
  */
 
-public interface ColumnReader extends ColumnAccessor {
+public interface ColumnReader {
+
+  ColumnMetadata schema();
 
   /**
-   * Report if the column is null. Non-nullable columns always
-   * return <tt>false</tt>.
-   * @return true if the column value is null, false if the
-   * value is set
+   * The type of this reader.
+   *
+   * @return type of reader
    */
+
+  ObjectType type();
+
+  /**
+   * Determine if this value is null.
+   * <ul>
+   * <li>Nullable scalar: determine if the value is null.</li>
+   * <li>Non-nullable scalar: always returns <tt>false</tt>.</li>
+   * <li>Arrays: always returns </tt>false</tt.></li>
+   * <li>Lists: determine if the list for the current row is null.
+   * In a list, an array entry can be null, empty, or can contain
+   * items. In repeated types, the array itself is never null.
+   * If the array is null, then it implicitly has no entries.</li>
+   * <li>Map or Repeated Map: Always returns <tt>false</tt>.</li>
+   * <li>Map inside a union, or in a list that contains a union,
+   * the tuple itself can be null.</li>
+   * <li>Union: Determine if the current value is null. Null values have no type
+   * and no associated reader.</li>
+   * </ul>
+   *
+   * @return <tt>true</tt> if this value is null; <tt>false</tt> otherwise
+   */
+
   boolean isNull();
-  int getInt();
-  long getLong();
-  double getDouble();
-  String getString();
-  byte[] getBytes();
-  BigDecimal getDecimal();
-  Period getPeriod();
+
+  /**
+   * Return the value of the underlying data as a Java object.
+   * Primarily for testing
+   * <ul>
+   * <li>Array: Return the entire array as an <tt>List</tt> of objects.
+   * Note, even if the array is scalar, the elements are still returned
+   * as a list.</li>
+   * </ul>
+   * @return the value as a Java object
+   */
+
   Object getObject();
-  TupleReader map();
-  ArrayReader array();
+
+  /**
+   * Return the entire object as a string. Primarily for debugging.
+   * @return string representation of the object
+   */
+
+  String getAsString();
 }
